9ab094cb715f12bae62079e7f203ea64f9626371
Chrisyilin <13661231863@163.com>
2025-07-14 15:56:56 +0800

    Create Class3.py

---
+++ Class3.py
@@ -0,0 +1,35 @@
+import this
+
+num=1
+print(num)
+s1=('🐯🦁🐬🦈123'
+    '=-@#%&¥')
+print(s1)
+
+s2='''1234df
+dfdj
+🐘🆚'''
+print(s2)
+
+s3="""test 32748
+📖🌞"""
+print(s3)
+
+# 定义字典
+d = {"abc":"123",1:9,'cd':0}
+print('d:',d)
+# 根据key获取字典的value
+print(d.get("abc"))
+# 获取字典的all values
+print(d.values())
+print(this.d)
+"""一条长注释"""
+ '''依旧长注释'''
+
+print(this.d)
+print(this.d)
+print(this.d)
+print(this.d)
+
+print(1)
+property
